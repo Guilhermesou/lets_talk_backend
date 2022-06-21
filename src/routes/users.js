b970e345a1dd09fc0092f5ac9e9fc0a1151ff659
Guilhermesou <guilhesou@gmail.com>
2022-06-20 22:42:20 -0400

    Add middleware authentication on user routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,13 @@
 import { Router} from "express";
 import users_controller from "../controllers/users-controller.js";
+import authMiddleware from "../middleware/auth.js";
 const router = Router();
 
-router.put('/:id', users_controller.update)
-router.delete('/:id', users_controller.remove)
-router.get('/', users_controller.getAll)
-router.put('/:id/follow', users_controller.follow)
-router.put('/:id/unfollow', users_controller.unfollow);
+router.put('/:id', [authMiddleware, users_controller.update])
+router.delete('/:id', [authMiddleware, users_controller.remove])
+router.get('/', [authMiddleware, users_controller.getAll])
+router.put('/:id/follow', [authMiddleware, users_controller.follow])
+router.put('/:id/unfollow', [authMiddleware, users_controller.unfollow]);
 
 /*
 router.put('/:id', async (req, res) => {
