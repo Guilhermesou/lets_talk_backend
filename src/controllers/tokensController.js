104cb03569f230c10a89ef563c7707bafafb9dcb
Guilhermesou <guilhesou@gmail.com>
2022-08-10 21:16:50 -0400

    Some functions of refresh tokens was added and some refactoring in the expiration with time in number and his unit and a better named export

--- src/controllers/tokensController.js
+++ src/controllers/tokensController.js
@@ -1,15 +1,20 @@
-import blocklist from "../../redis/blocklist.js";
 import jwt from "jsonwebtoken";
+import allowlist from "../../redis/allowlist.js";
+import blocklist from "../../redis/blocklist.js";
 
-function generateToken(params = {}) {
-    return jwt.sign(
-        { params },
+async function createToken(params = {}, [timeNumber, timeUnit]) {
+
+    const token = jwt.sign(
+        { ...params },
         process.env.SECRET,
-        { expiresIn: process.env.JWTTOKENEXPIRESIN }
+        { expiresIn: timeNumber + timeUnit}
     );
+
+    return token;
 }
 
 async function checkTokenJWTInBlocklist(token, name, blocklist) {
+    
     if (!blocklist) {
         return;
     }
@@ -25,8 +30,59 @@ async function checkTokenJWT(token, name, blocklist) {
     return id;
 }
 
-function expiresTokenJWT(token, blocklist) {
-    return blocklist.add(token);
+function expiresTokenJWT(token, blocklist, expiration) {
+    return blocklist.add(token, expiration);
 }
 
-export default {generateToken, checkTokenJWT, expiresTokenJWT}
+async function createOpacToken(id, [timeNumber, timeUnit], allowlist) {
+    const opacToken = crypto.randomBytes(24).toString('hex');
+    const expirationDate = moment().add(timeNumber, timeUnit).unix();
+    await allowlist.addTokenInRedis(opacToken, id, expirationDate);
+    return opacToken;
+}
+
+async function checkOpacToken(token, name, allowlist) {
+    if (!token) {
+        throw new Error('Token não enviado!');
+    }
+    const id = await allowlist.getValue(token);
+    if (!id) {
+        throw new Error('Token inválido!');
+    }
+    return id;
+}
+
+async function expiresOpacToken(token, allowlist) {
+    await allowlist.del(token);
+}
+
+export default {
+    access: {
+        name: "access token",
+        list: blocklist,
+        expiration: [15, 'm'],
+        create(params) {
+            return createToken(params, this.expiration);
+        },
+        check(token) {
+            return checkTokenJWT(token, this.name, this.list);
+        },
+        expires(token) {
+            return expiresTokenJWT(token, this.list, this.expiration);
+        }
+    },
+    refresh: {
+        name: "refresh token",
+        list: allowlist,
+        expiration: [5, 'd'],
+        create(id) {
+            return createOpacToken(id, this.expiração, this.list);
+        },
+        check(token) {
+            return checkOpacToken(token, this.name, this.list);
+        },
+        expires(token) {
+            return expiresOpacToken(token, this.list);
+        }
+    }
+}
